39a975353b68f180e1b29e1c401cf31e9e72ed7c
aaletov <yabadabadushka@gmail.com>
2022-05-20 02:48:34 +0100

    Task #3 Test 6 WA
    Signed-off-by: aaletov <yabadabadushka@gmail.com>

--- task_3/main.cpp
+++ task_3/main.cpp
@@ -5,12 +5,19 @@
 const int SIZE = 1999;
 const int SHIFT = 999;
 
+const char TRUE = '1';
+const char FALSE = '0';
+const char BEGIN = '(';
+const char END = ')';
+
 struct closed_interval_t {
   int begin_;
   int end_;
+  int intersects_;
   closed_interval_t(int begin, int end):
     begin_(begin + SHIFT),
-    end_(end + SHIFT)
+    end_(end + SHIFT),
+    intersects_(0)
   {}
   int get_unshifted_begin() {
     return begin_ - SHIFT;
@@ -20,59 +27,89 @@ struct closed_interval_t {
   }
 };
 
-bool does_have_inner_points(const std::vector< bool >& coverage, const closed_interval_t& interval) {
+bool is_point_in_interval(const closed_interval_t& interval, int point) {
+  return (point > interval.begin_) && (point < interval.end_);
+}
+
+bool is_two_pointed(const closed_interval_t& lhs) {
+  return lhs.end_ - lhs.begin_ == 1;
+}
+
+bool is_intersected(const closed_interval_t& lhs, const closed_interval_t& rhs) {
+  return is_point_in_interval(lhs, rhs.begin_) || is_point_in_interval(rhs, lhs.begin_)
+    || is_point_in_interval(lhs, rhs.end_) || is_point_in_interval(rhs, lhs.end_) 
+    || ((lhs.begin_ == rhs.begin_) && (lhs.end_ == rhs.end_));
+}
+
+bool does_have_inner_points(const std::vector< char >& coverage, const closed_interval_t& interval) {
+  if (is_two_pointed(interval) && !(((coverage[interval.begin_] == END) && (coverage[interval.end_ == BEGIN]))
+    || ((coverage[interval.begin_] == FALSE) && (coverage[interval.end_] == FALSE)))) {
+    return true;
+  }
+  // covers all cases with inner points but two-pointed intervals doesn't have inner points
   for (int i = interval.begin_ + 1; i < interval.end_; i++) {
-    if (coverage[i] == true) {
+    if (coverage[i] != FALSE) {
       return true;
     }
   }
   return false;
 }
 
-void add(std::vector< bool >& coverage, const closed_interval_t& interval) {
-  for (int i = interval.begin_; i < interval.end_ + 1; i++) {
-    coverage[i] = true;
+void add(std::vector< char >& coverage, const closed_interval_t& interval) {
+  coverage[interval.begin_] = BEGIN;
+  coverage[interval.end_] = END;
+
+  for (int i = interval.begin_ + 1; i < interval.end_; i++) {
+    coverage[i] = TRUE;
   }
 }
 
-bool length_comparator(const closed_interval_t& lhs, const closed_interval_t& rhs) {
-  return (lhs.end_ - lhs.begin_) < (rhs.end_ - rhs.begin_);
+void calculate_intersects(std::vector< closed_interval_t >& intervals) {
+  for (int i = 0; i < intervals.size() - 1; i++) {
+    for (int j = i + 1; j < intervals.size(); j++) {
+      if (is_intersected(intervals[i], intervals[j])) {
+        intervals[i].intersects_++;
+        intervals[j].intersects_++;
+      }
+    }
+  }
+}
+
+bool intersect_comparator(const closed_interval_t& lhs, const closed_interval_t& rhs) {
+  return lhs.intersects_ < rhs.intersects_;
 }
 
 bool coordinate_comparator(const closed_interval_t& lhs, const closed_interval_t& rhs) {
   return lhs.begin_ < rhs.begin_;
 }
 
-std::vector< closed_interval_t > to_intervals(const std::vector< bool >& coverage) {
-  std::vector< closed_interval_t > intervals = {};
+std::vector< closed_interval_t > task(std::vector< closed_interval_t >& intervals) {
+  std::vector< char > coverage(SIZE);
 
-  int begin = 0;
-  int end = 0;
+  for (int i = 0; i < SIZE; i++) {
+    coverage[i] = FALSE;
+  }
+  
+  calculate_intersects(intervals);
+  std::sort(intervals.begin(), intervals.end(), intersect_comparator);
 
-  for (int i = 0; i < coverage.size(); i++) {
-    if ((begin == 0) && (coverage[i] == 1)) {
-      begin = i - SHIFT;
-    }
-    if ((begin != 0) && (coverage[i] == 0)) {
-      end = i - 1 - SHIFT;
-      intervals.push_back(closed_interval_t(begin, end));
-      begin = 0;
-      end = 0;
+  std::vector< closed_interval_t > filtered_intervals = {};
+
+  for (int i = 0; i < intervals.size(); i++) {
+    if (!does_have_inner_points(coverage, intervals[i])) {
+      add(coverage, intervals[i]);
+      filtered_intervals.push_back(intervals[i]);
     }
   }
 
-  return intervals;
+  std::sort(filtered_intervals.begin(), filtered_intervals.end(), coordinate_comparator);
+
+  return filtered_intervals;
 }
 
 int main() {
   int N = 0;
   std::cin >> N;
-
-  std::vector< bool > coverage(SIZE);
-
-  for (int i = 0; i < SIZE; i++) {
-    coverage[i] = false;
-  }
 
   std::vector< closed_interval_t > intervals = {};
 
@@ -84,22 +121,11 @@ int main() {
     intervals.push_back(closed_interval_t(lpoint, rpoint));
   }
 
-  std::sort(intervals.begin(), intervals.end(), length_comparator);
-  std::vector< closed_interval_t > filtered_intervals = {};
-
-  for (int i = 0; i < N; i++) {
-    if (!does_have_inner_points(coverage, intervals[i])) {
-      add(coverage, intervals[i]);
-      filtered_intervals.push_back(intervals[i]);
-    }
-  }
-
-  std::sort(filtered_intervals.begin(), filtered_intervals.end(), coordinate_comparator);
+  std::vector< closed_interval_t > filtered_intervals = task(intervals);
 
   std::cout << filtered_intervals.size() << '\n';
   for (int i = 0; i < filtered_intervals.size(); i++) {
     std::cout << filtered_intervals[i].get_unshifted_begin() << ' ' << filtered_intervals[i].get_unshifted_end() 
               << '\n';
   }  
-
 }
