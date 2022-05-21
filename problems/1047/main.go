f5b2f91b69fdfb0714cf92b78f47d092ede30dca
aaletov <yabadabadushka@gmail.com>
2022-05-21 16:05:50 +0100

    1047 Change to double precision and print with 2-digits precision
    Signed-off-by: aaletov <yabadabadushka@gmail.com>

--- problems/1047/main.go
+++ problems/1047/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func readInitials() (n int, a0 float32, an1 float32, err error) {
+func readInitials() (n int, a0 float64, an1 float64, err error) {
 	_, err = fmt.Scanf("%d\n%g\n%g\n", &n, &a0, &an1)
 
 	if err != nil {
@@ -13,19 +13,11 @@ func readInitials() (n int, a0 float32, an1 float32, err error) {
 	return n, a0, an1, nil
 }
 
-func forwardStep() {
-
-}
-
-func backwardStep() {
-
-}
-
 func main() {
 	var (
 		n int
-		a0 float32
-		an1 float32
+		a0 float64
+		an1 float64
 		err error
 	)
 
@@ -36,18 +28,18 @@ func main() {
 	}
 
 	if n == 1 {
-		var c1 float32
+		var c1 float64
 		fmt.Scanf("%g\n", &c1)
 		fmt.Printf("%g\n", (a0 + an1) / 2 - c1)
 		return
 	}
 
-	var a []float32 = make([]float32, n + 2)
+	var a []float64 = make([]float64, n + 2)
 
 	a[0] = a0
 	a[n + 1] = an1
 
-	var c []float32 = make([]float32, n + 1)
+	var c []float64 = make([]float64, n + 1)
 
 	for i := 1; i < n + 1; i++ {
 		_, err = fmt.Scanf("%g\n", &c[i])
@@ -56,12 +48,12 @@ func main() {
 		}
 	}
 
-	var alpha []float32 = make([]float32, n + 2)
-	var beta []float32 = make([]float32, n + 2)
+	var alpha []float64 = make([]float64, n + 2)
+	var beta []float64 = make([]float64, n + 2)
 
-	var A float32 = -1
-	var B float32 = 2
-	var C float32 = -1
+	var A float64 = -1
+	var B float64 = 2
+	var C float64 = -1
 
 	alpha[2] = -C / B;
 	beta[2] = ((2 * c[1] + a[0]) / B)
@@ -78,5 +70,5 @@ func main() {
 		a[i] = alpha[i + 1] * a[i + 1] + beta[i + 1]
 	}
 
-	fmt.Printf("%g\n", a[1])
+	fmt.Printf("%.2f\n", a[1])
 }
