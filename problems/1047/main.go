a313c06a54a3ee218df4fe5103a613cdafb0ccaa
aaletov <yabadabadushka@gmail.com>
2022-05-21 19:34:05 +0100

    1047 AC Split code on forward and backward sweeps
    Signed-off-by: aaletov <yabadabadushka@gmail.com>

--- problems/1047/main.go
+++ problems/1047/main.go
@@ -21,11 +21,35 @@ func readInitials() (n int, a0 float64, an1 float64, err error) {
 	return
 }
 
-func forwardStep(current *row, next *row) {
+func forwardStep(current *row, next *row, A float64, B float64, C float64) {
 	next.Alpha = -C / (A * current.Alpha + B)
 	next.Beta = (current.F - A * current.Beta) / (A * current.Alpha + B)
 
-	return 
+	return
+}
+
+func forwardSweep(n int, rows []row) {
+	forwardStep(&rows[1], &rows[2], 0, B, C)
+	for i := 2; i < n; i++ {
+		forwardStep(&rows[i], &rows[i + 1], A, B, C)
+	}
+	forwardStep(&rows[n], &rows[n + 1], A, B, 0)
+
+	return
+}
+
+func backwardStep(current *row, next *row) {
+	current.A = next.Alpha * next.A + next.Beta
+
+	return
+}
+
+func backwardSweep(n int, rows []row) {
+	for i := n; i > 0; i-- {
+		backwardStep(&rows[i], &rows[i + 1])
+	}
+
+	return
 }
 
 func main() {
@@ -70,17 +94,8 @@ func main() {
 	rows[1].F = -2 * c1 + rows[0].A
 	rows[n].F = -2 * cn + rows[n + 1].A
 
-	for i := 1; i < n; i++ {
-		rows[i + 1].Alpha = -C / (A * rows[i].Alpha + B)
-		rows[i + 1].Beta = (rows[i].F - A * rows[i].Beta) / (A * rows[i].Alpha + B)
-  }
-
-	rows[n + 1].Alpha = 0
-	rows[n + 1].Beta = ((rows[n].F - A * rows[n].Beta) / (A * rows[n].Alpha + B))  
-
-	for i := n; i > 0; i-- {
-		rows[i].A = rows[i + 1].Alpha * rows[i + 1].A + rows[i + 1].Beta
-	}
+	forwardSweep(n, rows)
+	backwardSweep(n, rows)
 
 	fmt.Printf("%.2f\n", rows[1].A)
 }
